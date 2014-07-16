1e8cc372c60798258be2781f61c849b33b9d9b81
letrunghieuvina <le.trung.hieu.vina@gmail.com>
2014-07-16 14:16:19 +0700

    add chap5.c and ex1_readNames.c

--- chap5.c
+++ chap5.c
@@ -1,85 +1,62 @@
-#include <string.h>
 #include <stdio.h>
 
-#define NAMENUM 3
-#define NAMELENGTH 10
-
-//char* names[NAMENUM];
-
-void readNames(char *(*names)[NAMENUM])
-{
-	//char* mnames[] = (char*[])(*names);
-	
-	printf("\n----------------------------- in readNames ^^! -------------------------------\n");
-	printf("address of pointer to names before init = %p\n", &names);
-	printf("value of names before init = %p\n", names);
-	printf("address of each item in names before init:\n");
-	for(int i = 0; i < NAMENUM; ++i)
-	{
-		printf("address of names[%d] = %p: %s\n", i, &(*names[i]), (*names)[i]);
-	}
-	printf("\n----------------------------- free old memory and allocate new memory -------------------------------\n");
-	for(int i = 0; i < NAMENUM; ++i)
-	{
-		free((*names)[i]);
-		(*names)[i] = (char*) malloc(NAMELENGTH);
-	}
-	for(int i = 0; i < NAMENUM; ++i)
-	{
-		printf("Enter name num#%d:", i);
-		scanf("%s", (*names)[i]);
-		//(*names)[i] = (char*) malloc(NAMELENGTH);
-	}
-}
-
-
+void readNames(char*** nameArray, int numberOfNames, int nameLength);
 
 int main()
 {
-	char *names[NAMENUM];
-	printf("address of names before init = %p\n", &names);
-	printf("address of each item in names before init:\n");
-	for(int i = 0; i < NAMENUM; ++i)
+	int numberOfNames = 0; 
+	int nameLength = 20;
+	char** namesArray;
+	
+	printf("How many names to input? ");
+	scanf("%i", &numberOfNames);
+
+	readNames(&namesArray, numberOfNames, nameLength);	
+
+	/*
+	namesArray = (char**) malloc(sizeof(char*) * numberOfNames);
+	
+	for(int i = 0; i < numberOfNames; ++i)
 	{
-		printf("address of names[%d] = %p\n", i, &names[i]);
+		namesArray[i] = (char*) malloc(nameLength);
+	}
+
+	for(int i = 0; i < numberOfNames; ++i)
+	{
+		printf("Input name #%i: ", i);
+		scanf("%s", namesArray[i]);
+	}
+	*/
+	printf("\nNow show the names input.\n");	
+
+	for(int i = 0; i < numberOfNames; ++i)
+	{
+		printf("name #%i = %s\n", i, namesArray[i]);
 	}
 	
-	for(int i = 0; i < NAMENUM; ++i)
+	printf("\nNow de-allocate memory");
+
+	for(int i = 0; i < numberOfNames; ++i)
 	{
-		names[i] = (char*) malloc(NAMELENGTH);
+		free(namesArray[i]);
 	}
-	printf("\n-----------------------------\n");
-	printf("address of names after init = %p\n", &names);
-	printf("address of each item in names after init:\n");
-	for(int i = 0; i < NAMENUM; ++i)
+
+	free(namesArray);
+
+	printf("\ndone");
+}
+
+void readNames(char*** nameArray, int numberOfNames, int nameLength)
+{
+	*nameArray = (char**) malloc(sizeof(char*) * numberOfNames);
+	for(int i = 0; i < numberOfNames; ++i)
 	{
-		printf("address of names[%p] = %p\n", i, &names[i]);
+		(*nameArray)[i] = (char*) malloc(nameLength);
 	}
-	for(int i = 0; i < NAMENUM; ++i)
+
+	for(int i = 0; i < numberOfNames; ++i)
 	{
-		printf("Enter name num#%d:", i);
-		scanf("%s", names[i]);
-		//printf("\n");
+		printf("Enter name #%i: ", i);
+		scanf("%s", (*nameArray)[i]);
 	}
-	printf("\n-----------------------------\n");
-	printf("address of names after scanf = %p\n", &names);
-	printf("address & value of each item in names after scanf:\n");
-	for(int i = 0; i < NAMENUM; ++i)
-	{
-		printf("address of names[%d] = %p: %s\n", i, &names[i], names[i]);
-	}
-	printf("\n-----------------------------\n");
-	
-	readNames(&names);
-	printf("\n----------------------------- print data after readNames() -------------------------------\n");
-	for(int i = 0; i < NAMENUM; i++)
-	{
-		printf("%s\n", names[i]);
-	}
-	for(int i = 0; i < NAMENUM; i++)
-	{
-		free(names[i]);
-	}
-	printf("\nend of main()");
-	return 0;
 }
