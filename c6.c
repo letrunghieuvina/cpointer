8bcf0bba1b6e1d17aa83079aac5297d237416151
letrunghieuvina <le.trung.hieu.vina@gmail.com>
2014-07-22 14:48:21 +0700

    linked list

--- c6.c
+++ c6.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 
+#define NAME_LENGTH 30
+
+typedef struct _employee {
+	char* name;
+	int age;	
+} Employee;
+
+typedef struct _student {
+	char* name;
+	int age;
+	char* class;
+} Student;
+
 typedef struct _node {
 	void* data;
 	struct _node* next;
@@ -12,8 +25,14 @@ typedef struct _linkedList {
 	Node* current;
 } LinkedList;
 
+typedef void (*DISPLAY)(void* data);
+
+Employee* initEmployee(Employee* employee, char* name, int age);
+
+Student* initStudent(Student* student, char* name, int age, char* class);
+
 // This initializes a LinkedList, set all fields to NULL
-void initializeList(LinkedList* list);
+void initLinkedList(LinkedList* list);
 
 // add data to head of the list
 void addHead(LinkedList* list, void* data);
@@ -29,13 +48,17 @@ Node* getNode(LinkedList* list, COMPARE compare, void* data);
 
 void displayLinkedList(LinkedList* list, DISPLAY display);
 
-int main() {
+void displayEmployee(Employee* employee);
 
+void displayStudent(Student* student);
+
+int main() {
+	
 }
 
 
 // This initializes a LinkedList, set all fields to NULL
-void initializeList(LinkedList* list){
+void initLinkedList(LinkedList* list){
 	list->head = NULL;
 	list->tail = NULL;
 	list->current = NULL;
@@ -71,20 +94,71 @@ void addTail(LinkedList* list, void* data){
 // this also free the allocated memory of node (but node node->data).
 void delete(LinkedList* list, Node* node) {
 	Node* temp = list->head;
-	if(temp == node) {
+	if(node == list->head) {
 		//case there is only 1 item in the linked list
 		if(list->head == list->tail) {
-			
+			list->head = NULL;
+			list->tail = NULL;
+		} else {
+			list->head = list->head->next;
 		}
-		
 	} else {
 		while(temp != NULL && temp->next != node) {
-			
+			temp = temp->next;
+		}
+		if(temp != NULL) {
+			temp->next = node->next;
 		}
 	}
+	free(node);
 }
 
 // get a node in the list
-Node* getNode(LinkedList* list, COMPARE compare, void* data);
+Node* getNode(LinkedList* list, COMPARE compare, void* data){
+	return NULL;
+}
 
+void displayLinkedList(LinkedList* list, DISPLAY display) {
+	if(list == NULL) {
+		return;
+	}
+	printf("\nLinked List\n");
+	Node* current = list->head;
+	while(current != NULL) {
+		display(current->data);
+		current = current->next;
+	} 	
+}
+
+void displayEmployee(Employee* employee){
+	if(employee != NULL) {
+		printf("Employee: name = %s, age = %i\n", employee->name, employee->age);
+	}
+}
+
+void displayStudent(Student* student) {
+	if(student != NULL) {
+		printf("Student: name = %s, age = %i, class = %s\n", student->name, student->age, student.class);
+	}
+}
+
+Employee* initEmployee(Employee* employee, char* name, int age) {
+	employee = (Employee*) malloc(sizeof(Employee));
+	employee->age = age;
+	employee->name = (char*) malloc(NAME_LENGTH + 1);
+	strcpy(employee->name, name);
+	
+	return employee;
+}
+
+Student* initStudent(Student* student, char* name, int age, char* class) {
+	student = (Student*) malloc(sizeof(Student));
+	student->name = (char*) malloc(NAME_LENGTH + 1);
+	student->class = (char*) malloc(NAME_LENGTH + 1);
+	strcpy(student->name, name);
+	strcpy(student->class, class);
+	student->age = age;
+
+	return student;
+}
 
